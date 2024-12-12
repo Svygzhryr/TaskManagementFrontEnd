02fa8b90b85b40f3b67455455d344b7fd6905231
Pavel Mihailov <pamixy@gmail.com>
2024-12-12 11:05:41 +0300

    feat: add home page and basic style setup

---
+++ src/main.tsx
@@ -0,0 +1,26 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+
+import "./reset.css";
+import "./index.css";
+
+import Login from "./views/Login";
+import Register from "./views/Register";
+import Home from "./views/Home";
+import NotFound from "./views/NotFound";
+import { BrowserRouter, Route, Routes } from "react-router";
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <BrowserRouter>
+      <div>
+        <Routes>
+          <Route path="*" element={<NotFound />} />
+          <Route index path="/" element={<Home />} />
+          <Route path="register" element={<Register />} />
+          <Route path="login" element={<Login />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  </StrictMode>,
+);
---
+++ src/views/Home.tsx
@@ -0,0 +1,29 @@
+import { Link } from "react-router";
+
+export default function Login() {
+  return (
+    <>
+      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-center">
+        {/* <h3>Welcome, authorized user!</h3> */}
+        <h3 className="mb-10 text-neutral-200 text-3xl">
+          You should authorize first
+        </h3>
+        <div className="flex justify-evenly">
+          <Link
+            to="register"
+            className="w-32 px-4 py-2 border-2 border-solid border-transparent hover:text-red-400 hover:border-red-400 transition-all"
+          >
+            Sign up
+          </Link>
+          <Link
+            to="login"
+            className="w-32 px-4 py-2 border-2 border-solid border-transparent hover:text-blue-400 hover:border-blue-400 transition-all
+            "
+          >
+            Login
+          </Link>
+        </div>
+      </div>
+    </>
+  );
+}
